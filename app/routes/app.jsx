4640567f47e8c184b2b2e9bc24854f13084da7d8
zicaiw625 <zicaiw625@gmail.com>
2025-11-21 16:08:39 +0800

    revert

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -68,7 +68,7 @@ export default function App() {
   return (
     <AppProvider embedded apiKey={apiKey}>
       <ShopifyFetchProvider>
-        <div role="navigation" style={navStyle}>
+        <s-app-nav style={navStyle}>
           <LinkWithQuery to="/app" style={linkStyle}>
             {t(TRANSLATION_KEYS.NAV_OVERVIEW)}
           </LinkWithQuery>
@@ -90,7 +90,7 @@ export default function App() {
           <div style={{ marginLeft: "auto" }}>
             <LanguageToggle />
           </div>
-        </div>
+        </s-app-nav>
         <Outlet />
       </ShopifyFetchProvider>
     </AppProvider>
